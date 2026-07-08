68c32bca9bdf22716baa527b22ae9f8e36ecb5a1
Nivedh E R <nivedher12@gmail.com>
2026-07-08 22:25:25 +0530

    feat(Navbar): implemented the Navbar and it's styles and functionalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router";
+import "../styles/Navbar.css";
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="navbar__container">
+      <div className="navbar__heading-container">
+        <button className="navbar__heading-btn" title="Shopo Home">
+          <Link to={"/home"} className="navbar__heading-link">
+            <h1>Shopo</h1>
+          </Link>
+        </button>
+      </div>
+      <div className="navbar__link-container">
+        <Link to={"home"} className="navbar__home-link">
+          Home
+        </Link>
+        <Link to={"products"} className="navbar__products-link">
+          Products
+        </Link>
+        <Link to={"cart"} className="navbar__cart-link">
+          Cart
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
