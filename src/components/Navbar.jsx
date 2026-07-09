bf4936f9f48c4cac2b07de601e1aa91aaff773aa
Nivedh E R <nivedher12@gmail.com>
2026-07-09 21:03:33 +0530

    style(active): implmented active style effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,45 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar__container">
       <div className="navbar__heading-container">
-        <Link to={"/home"} className="navbar__heading-link" title="Shopo Home">
+        <NavLink
+          to={"/home"}
+          className="navbar__heading-link"
+          title="Shopo Home"
+        >
           <h1>Shopo</h1>
-        </Link>
+        </NavLink>
       </div>
       <ul className="navbar__link-container">
         <li>
-          <Link
+          <NavLink
             to={"home"}
             className="navbar__home-link hover-underline"
             title="Home"
           >
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to={"products"}
             className="navbar__products-link hover-underline"
             title="Products"
           >
             Products
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
+          <NavLink
             to={"cart"}
             className="navbar__cart-link hover-underline"
             title="Cart"
           >
             Cart
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
