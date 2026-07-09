376fe6b6054b2511b04cbf4d63799c3f9a746a7e
Nivedh E R <nivedher12@gmail.com>
2026-07-09 20:31:07 +0530

    feat(HomePage): implemented the homepage styles and design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
-import { useOutletContext } from "react-router";
+import { Link } from "react-router";
 import "../styles/Home.css";
 
 const Home = () => {
-  const { data } = useOutletContext();
   return (
-    <div>
-      {data.products.map((product) => {
-        return <p key={product.id}>{product.title}</p>;
-      })}
+    <div className="home__container">
+      <div className="home__heading-container">
+        <h1 className="home__heading-new-arrivals">NEW ARRIVALS</h1>
+        <div className="home__heading-just-for-container">
+          <h1 className="home__heading-just">JUST</h1>
+          <h1 className="home__heading-for">FOR</h1>
+        </div>
+        <h1 className="home__heading-you">you</h1>
+      </div>
+      <Link to={"/products"} className="home__products-link">
+        Go to Products &rarr;
+      </Link>
     </div>
   );
 };
-
 export default Home;
