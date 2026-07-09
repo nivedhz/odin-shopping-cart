4083d0e018615f74603b2b6cc27a5b4600c5f2ba
Nivedh E R <nivedher12@gmail.com>
2026-07-09 21:03:53 +0530

    feat(Products): implemented the Products page and linked it to the router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
+import Products from "../pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "home", element: <Home /> }],
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "products", element: <Products /> },
+    ],
     errorElement: <Error />,
   },
 ]);
