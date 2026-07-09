57412a96a7c0a608792fef9004df79f895da781e
Nivedh E R <nivedher12@gmail.com>
2026-07-09 15:12:35 +0530

    feat(Loading): implemented a loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,20 @@ import { useProducts } from "./data/fetchProducts.jsx";
 
 function App() {
   const [data, error, loading] = useProducts();
+  if (loading)
+    return (
+      <div className="loader__loading-container">
+        <div className="inner one"></div>
+        <div className="inner two"></div>
+        <div className="inner three"></div>
+      </div>
+    );
+  if (error) return <h1>Error: {error}</h1>;
   return (
     <>
       <Navbar />
       <Navigate to={"/home"} replace />
-      <Outlet context={{ data, error, loading }} />
+      <Outlet context={{ data }} />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import { useOutletContext } from "react-router";
+import "../styles/Home.css";
 
 const Home = () => {
-  const { data, error, loading } = useOutletContext();
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error: {error}</h1>;
+  const { data } = useOutletContext();
   return (
     <div>
       {data.products.map((product) => {
